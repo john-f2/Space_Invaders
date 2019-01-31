bc98bb0ef5d214c5cfef30601ef587e35132f17c
John Fu <fujohn2@hotmail.com>
2019-01-30 19:16:02 -0800

    Began working on basic spaceship scripts, and ui objects
    spaceship can now move left and right, and will be stoped by two barrier objects, added in UI objects but they do not have any functionality

---
+++ Assets/Scripts/shipMovementScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 
+ * ship movement script 
+ * Space Invaders Project
+ * 
+ * 
+ */
+public class shipMovementScript : MonoBehaviour
+{
+
+    [SerializeField] protected float playerSpeed = 5.0f;
+
+    private Rigidbody2D shipRigidBody;
+    private float xMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gets the ship's RigidBody2D, used for movement 
+        shipRigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gets the x movement from the horizontal axis
+        //applies velocity to the ship based on the xMovement 
+        xMovement = Input.GetAxis("Horizontal");
+        shipRigidBody.velocity = new Vector2(xMovement * playerSpeed, shipRigidBody.velocity.y);
+    }
+}
