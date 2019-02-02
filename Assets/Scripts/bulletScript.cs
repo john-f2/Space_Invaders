d0c9610e8ae3e09a7943ece975a793cdd6e9e95f
John Fu <fujohn2@hotmail.com>
2019-02-01 18:53:47 -0800

    added in a basic enemy
    enemy will die when bullet hits them

--- Assets/Scripts/bulletScript.cs
+++ Assets/Scripts/bulletScript.cs
@@ -22,4 +22,22 @@ public class bulletScript : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //if the bullet hits the "bullet net" game object
+        //it will destory the bullet
+        //reduces the amount of stray bullets
+        if (col.CompareTag("bulletNet"))
+        {
+            Destroy(this.gameObject);
+        }
+        if (col.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy has been hit");
+            //when enemy is hit destory the gameObject
+            Destroy(col.gameObject);
+        }
+
+    }
 }
