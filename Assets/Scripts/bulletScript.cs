35332798d9c62e913b0a013e7878105dad2c9f6c
John Fu <fujohn2@hotmail.com>
2019-02-01 18:30:56 -0800

    Implemented Bullet and a basic bullet Script
    now the space ship can shoot when pressing space. bullet moves using the bullet script

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    //speed variable, used to adjust the speed of the bullet 
+    [SerializeField] protected float bulletSpeed = 5.0f;
+
+    private Rigidbody2D bulletRigidBody; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+        bulletRigidBody.velocity = Vector2.up * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/shipShootingScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Ship shooting script
+ * used to instantiate a new bullet instance 
+ * 
+ * 
+ * 
+ */
+public class shipShootingScript : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot();
+
+        }
+    }
+
+    void shoot()
+    {
+        //instantiates the bullet object
+        //the bullet object will have a script attacted 
+        //that will run movement on start()
+        float x = this.transform.position.x;
+        //this y position allows the bullet to come out at the front of the ship
+        //it needs to be adjusted if we change the sprite
+        float y = this.transform.position.y + .5f;
+        Vector2 bulletPosition = new Vector2(x,y);
+        Instantiate(bulletPrefab, bulletPosition, Quaternion.identity);
+        
+    }
+}
