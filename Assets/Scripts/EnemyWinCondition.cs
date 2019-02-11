8213f4b1fd7bb779b731c4e10c85d1256eede2d2
milkcloud <32286235+milkcloud@users.noreply.github.com>
2019-02-11 05:26:49 -0800

    Game ends when enemies reach bottom.

---
+++ Assets/Scripts/EnemyWinCondition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyWinCondition : MonoBehaviour
+{
+    //private void OnTriggerEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("1");
+    //    if (collision.collider.CompareTag("Enemy"))
+    //    {
+    //        Debug.Log("2");
+    //        SceneManager.LoadScene("EndMenu");
+    //    }
+    //}
+
+    //private void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    Debug.Log("1");
+    //    if (col.CompareTag("Enemy"))
+    //    {
+
+    //        Debug.Log("2");
+    //        SceneManager.LoadScene("EndMenu");
+    //    }
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("1");
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            Debug.Log("2");
+            SceneManager.LoadScene("EndMenu");
+        }
+    }
+}
